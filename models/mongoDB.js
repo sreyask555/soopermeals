8d7fb316a25ca800db133073c63d88ac85d20bc9
sreyask555 <sreyask555@gmail.com>
2024-03-21 15:30:10 +0530

    fix : mongo uri update

--- models/mongoDB.js
+++ models/mongoDB.js
@@ -1,7 +1,20 @@
 const mongoose = require('mongoose');
 
+// Read values from .env
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Mongo uri fetch from .env
+let MONGO_URI;
+if (process.env.NODE_ENV === 'development'){
+    MONGO_URI = process.env.MONGO_DEV_URI;
+}
+else if(process.env.NODE_ENV === 'production'){
+    MONGO_URI = process.env.MONGO_PROD_URI;
+}
+
 // MongoDB-Node Connection
-mongoose.connect(`mongodb://${process.env.MONGO_SERVER}`)
+mongoose.connect(MONGO_URI)
 .then(()=>{
     console.log('MongoDB Connected for Schemas');
 })
@@ -10,4 +23,4 @@ mongoose.connect(`mongodb://${process.env.MONGO_SERVER}`)
     console.error(err.stack)
 });
 
-module.exports = mongoose;
+module.exports = mongoose; 
